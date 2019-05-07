ecc8cdafea499ef15b05e54812da85571862f801
Dan Gilbert <daniel.gilbert0051@gmail.com>
2019-05-07 16:30:17 -0400

    task: setup map with marker and centering

--- android/app/src/main/java/com/learningrn/MainApplication.java
+++ android/app/src/main/java/com/learningrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.learningrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.reactnativenavigation.NavigationReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new NavigationReactPackage(),
             new VectorIconsPackage()
       );
--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
+import MapView from 'react-native-maps'; // Can name this whatever, it is the default export of r-n-m
 
 class PickLocation extends Component {
+  state = {
+    focusedLocation: {
+      latitude: 37.7900352,
+      longitude: -122.4013726,
+      latitudeDelta: 0.0122,
+      longitudeDelta:
+        (Dimensions.get('window').width / Dimensions.get('window').height) *
+        0.0122
+    },
+    locationChosen: false
+  };
+
+  pickLocationHandler = evt => {
+    const coords = evt.nativeEvent.coordinate;
+    this.setState({
+      focusedLocation: {
+        ...this.state.focusedLocation,
+        latitude: coords.latitude,
+        longitude: coords.longitude
+      },
+      locationChosen: true
+    });
+  };
+
   render() {
+    const { focusedLocation, locationChosen } = this.state;
+    let marker = null;
+
+    if (locationChosen) {
+      marker = <MapView.Marker coordinate={focusedLocation} />;
+    }
+
     return (
       <View style={styles.container}>
-        <View style={styles.placeholder}>
-          <Text>Map</Text>
-        </View>
+        <MapView
+          initialRegion={focusedLocation}
+          region={focusedLocation}
+          style={styles.map}
+          onPress={this.pickLocationHandler}
+        >
+          {marker}
+        </MapView>
         <View style={styles.button}>
           <Button title="Locate Me" onPress={() => alert('Pick Location!')} />
         </View>
@@ -21,13 +58,11 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center'
   },
-  placeholder: {
-    borderWidth: 1,
-    borderColor: 'black',
-    backgroundColor: '#eee',
+  map: {
     width: '80%',
-    height: 150,
-    alignItems: 'center'
+    height: 250,
+    borderColor: 'black',
+    borderWidth: 0.5
   },
   button: {
     margin: 8

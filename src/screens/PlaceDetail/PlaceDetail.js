0ec12da4bd84d73d95bf91438b683556594c8c9c
Dan Gilbert <daniel.gilbert0051@gmail.com>
2019-05-07 18:06:09 -0400

    task: added map to placeDetail, reponsive to horizontal or vert view

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -10,6 +10,7 @@ import {
 import Icon from 'react-native-vector-icons/Ionicons';
 import { connect } from 'react-redux';
 import { deletePlace } from '../../store/actions/places';
+import MapView from 'react-native-maps';
 
 class PlaceDetailScreen extends Component {
   state = {
@@ -39,16 +40,32 @@ class PlaceDetailScreen extends Component {
   render() {
     const { place } = this.props;
     const { view } = this.state;
+    const fullLocation = {
+      ...place.location,
+      latitudeDelta: 0.0122,
+      longitudeDelta: 0.000001
+    };
+
     return (
       <View style={styles[`${view}Container`]}>
-        <Image source={place.image} style={styles[`${view}PlaceImage`]} />
-        <View>
+        <View style={styles.heading}>
           <Text style={styles.placeName}>{place.name}</Text>
-          <TouchableOpacity onPress={() => this.placeDeletedHandler(place.key)}>
-            <View style={styles.deleteButton}>
-              <Icon size={30} name="ios-trash" color="red" />
-            </View>
+          <TouchableOpacity
+            style={styles.deleteBtn}
+            onPress={() => this.placeDeletedHandler(place.key)}
+          >
+            <Icon size={30} name="ios-trash" color="red" />
           </TouchableOpacity>
+        </View>
+        <View style={styles[`${view}ImageAndMap`]}>
+          <Image source={place.image} style={styles[`${view}PlaceImage`]} />
+          <MapView
+            style={styles[`${view}Map`]}
+            initialRegion={fullLocation}
+            region={fullLocation}
+          >
+            <MapView.Marker coordinate={place.location} />
+          </MapView>
         </View>
       </View>
     );
@@ -57,33 +74,58 @@ class PlaceDetailScreen extends Component {
 
 const styles = StyleSheet.create({
   vertContainer: {
+    flex: 1,
     margin: 50,
     flexDirection: 'column',
     justifyContent: 'flex-start'
   },
-  horiContainer: {
-    marginTop: 50,
+  heading: {
     flexDirection: 'row',
-    width: '100%',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'space-evenly'
+    width: '100%',
+    margin: 5
+  },
+  horiImageAndMap: {
+    flexDirection: 'row'
+  },
+  vertImageAndMap: {
+    flexDirection: 'column'
+  },
+  horiContainer: {
+    width: '100%',
+    alignItems: 'center'
   },
   vertPlaceImage: {
     width: '100%',
     height: 200
   },
   horiPlaceImage: {
-    width: '50%',
+    width: '45%',
     height: 200,
     marginRight: 15
   },
   placeName: {
     fontWeight: 'bold',
     textAlign: 'center',
-    fontSize: 30
+    fontSize: 30,
+    marginRight: 10
   },
-  deleteButton: {
-    alignItems: 'center'
+  deleteBtn: {
+    marginLeft: 10
+  },
+  horiMap: {
+    width: '45%',
+    height: 200,
+    borderColor: 'black',
+    borderWidth: 0.5
+  },
+  vertMap: {
+    width: '100%',
+    height: 250,
+    borderColor: 'black',
+    borderWidth: 0.5,
+    marginTop: 5
   }
 });
 

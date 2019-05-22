defe95871a739b7ab0e6092cb9968b186fafeb6a
Dan Gilbert <daniel.gilbert0051@gmail.com>
2019-05-22 13:52:43 -0400

    feat: succesfully adds image to firebase

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,9 @@ const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
 const fs = require('fs');
 const UUID = require('uuid-v4');
-const { projId, bucketLoc } = require('../secrets');
+
+const projId = 'learningrn-40203';
+const bucketLoc = 'learningrn-40203.appspot.com';
 
 const gcConfig = {
   projectId: projId,
@@ -14,9 +16,9 @@ const gcs = require('@google-cloud/storage')(gcConfig);
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.storeImage = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
+  return cors(req, res, () => {
     const body = JSON.parse(req.body);
-    fs.writeFileSync('/tmp/uploaded-image-jpg', body.image, 'base64', err => {
+    fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', err => {
       console.log(err);
       return res.status(500).json({ error: err });
     });
@@ -24,13 +26,76 @@ exports.storeImage = functions.https.onRequest((req, res) => {
     const bucket = gcs.bucket(bucketLoc);
     const uniqueId = UUID();
 
-    bucket.upload('/tmp/uploaded-image-jpg', {
-      uploadType: 'media',
-      destination: `/places/${uniqueId}.jpg`,
-      metaData: {
-        contentType: 'image/jpeg',
-        firebaseStorageDownloadTokens: uniqueId
+    return bucket.upload(
+      '/tmp/uploaded-image.jpg',
+      {
+        uploadType: 'media',
+        destination: `/places/${uniqueId}.jpg`,
+        metadata: {
+          metadata: {
+            contentType: 'image/jpeg',
+            firebaseStorageDownloadTokens: uniqueId
+          }
+        }
+      },
+      (err, file) => {
+        if (!err) {
+          res.status(201).json({
+            imageUrl:
+              'https://firebasestorage.googleapis.com/v0/b/' +
+              bucket.name +
+              '/o/' +
+              encodeURIComponent(file.name) +
+              '?alt=media&token=' +
+              uniqueId
+          });
+        } else {
+          console.log(err);
+          res.status(500).json({ error: err });
+        }
       }
-    });
+    );
   });
 });
+
+// exports.storeImage = functions.https.onRequest((request, response) => {
+//   return cors(request, response, () => {
+//     const body = JSON.parse(request.body);
+//     fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', err => {
+//       console.log(err);
+//       return response.status(500).json({ error: err });
+//     });
+//     const bucket = gcs.bucket(projId + '.appspot.com');
+//     const uuid = UUID();
+
+//     return bucket.upload(
+//       '/tmp/uploaded-image.jpg',
+//       {
+//         uploadType: 'media',
+//         destination: '/places/' + uuid + '.jpg',
+//         metadata: {
+//           metadata: {
+//             contentType: 'image/jpeg',
+//             firebaseStorageDownloadTokens: uuid
+//           }
+//         }
+//       },
+//       (err, file) => {
+//         if (!err) {
+//           return response.status(201).json({
+//             imageUrl:
+//               'https://firebasestorage.googleapis.com/v0/b/' +
+//               bucket.name +
+//               '/o/' +
+//               encodeURIComponent(file.name) +
+//               '?alt=media&token=' +
+//               uuid
+//           });
+//         } else {
+//           console.log(err);
+//           return response.status(500).json({ error: err });
+//         }
+//       }
+//     );
+//   });
+// });
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,37 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
-export const addPlace = (placeName, location, image) => ({
-  type: ADD_PLACE,
-  placeName,
-  location,
-  image
-});
+export const addPlace = (placeName, location, image) => {
+  return dispatch => {
+    const placeData = {
+      placeName,
+      location
+    };
+    fetch(
+      'https://us-central1-learningrn-40203.cloudfunctions.net/storeImage',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          image: image.base64
+        })
+      }
+    )
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log('parsed response: ', parsedRes);
+      });
+    //   fetch('https://learningrn-40203.firebaseio.com/places.json', {
+    //     method: 'POST',
+    //     body: JSON.stringify(placeData)
+    //   })
+    //     .catch(err => console.log(err))
+    //     .then(res => res.json())
+    //     .then(parsedRes => {
+    //       console.log(parsedRes);
+    //     });
+    // };
+  };
+};
 
 export const deletePlace = placeKey => ({
   type: DELETE_PLACE,
